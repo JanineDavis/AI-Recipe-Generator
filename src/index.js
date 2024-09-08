026a25cdc601d46d4616d66fe69d89d9e8140813
JanineDavis <122249027+JanineDavis@users.noreply.github.com>
2024-09-08 15:53:00 +1000

    Added basic JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "Recipe",
+    autoStart: true,
+    delay: 2,
+    cursor: "",
+  });
+  recipeElement.innterHTML = "Recipe";
+
+  alert("Generating recipe");
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator");
+recipeFormElement.addEventListener("submit", generateRecipe);
